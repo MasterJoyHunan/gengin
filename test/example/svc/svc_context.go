ec5078f25ce14892ef006e86dac19f1fa5d063f7
Master Joy <386442876@qq.com>
2023-05-30 16:03:16 +0800

    优化: svc_context 实现 context.Context 接口

--- test/example/svc/svc_context.go
+++ test/example/svc/svc_context.go
@@ -1,9 +1,12 @@
 package svc
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
+// ServiceContext 实现 context.Context 接口
 type ServiceContext struct {
 	GinContext *gin.Context
 }
@@ -12,4 +15,20 @@ func NewServiceContext(c *gin.Context) *ServiceContext {
 	return &ServiceContext{
 		GinContext: c,
 	}
+}
+
+func (c *ServiceContext) Deadline() (deadline time.Time, ok bool) {
+	return c.GinContext.Deadline()
+}
+
+func (c *ServiceContext) Done() <-chan struct{} {
+	return c.GinContext.Done()
+}
+
+func (c *ServiceContext) Err() error {
+	return c.GinContext.Err()
+}
+
+func (c *ServiceContext) Value(key any) any {
+	return c.GinContext.Value(key)
 }
